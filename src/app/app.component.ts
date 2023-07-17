d86794bf7386b47f917bfb31f9831e6f35c11f68
AlibayOsmanli <aosmanli20@ku.edu.tr>
2023-07-17 12:44:39 +0400

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'AdminPanel';
+  title = 'admin-panel-layout';
+  sideBarOpen = false;
+
+  sideBarToggler() {
+    this.sideBarOpen = !this.sideBarOpen;
+  }
 }
